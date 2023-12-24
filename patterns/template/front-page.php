26ebd10a5e293fc2028534a1ece2700770e86eda
SEO Themes <seothemeswp@gmail.com>
2023-12-24 18:23:00 +0800

    logos

--- patterns/template/front-page.php
+++ patterns/template/front-page.php
@@ -10,7 +10,7 @@
 <!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header","style":{"position":{"all":"absolute"},"top":{"all":"0px"},"right":{"all":"0px"},"bottom":{"all":""},"left":{"all":"0px"},"zIndex":{"all":"2"}}} /-->
 <!-- wp:group {"tagName":"main","style":{"zIndex":{"all":"1"},"position":{"all":"relative"}},"className":"site-main alignfull","layout":{"type":"constrained"},"metadata":{"name":"Main"}} -->
 <main class="wp-block-group site-main alignfull"><!-- wp:pattern {"slug":"hero-default"} /-->
-<!-- wp:pattern {"slug":"testimonial-logos"} /-->
+<!-- wp:blockify/pattern {"slug":"testimonial-brands"} /-->
 <!-- wp:pattern {"slug":"feature-icons"} /-->
 <!-- wp:pattern {"slug":"feature-bento"} /-->
 <!-- wp:pattern {"slug":"faq-sticky"} /-->
---
+++ patterns/testimonial/brands.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: Testimonial Brands
+ * Slug: brands
+ * Categories: testimonial
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"},"margin":{"top":"-1px","bottom":"-1px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:-1px;margin-bottom:-1px;padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary-300"}}}},"textColor":"primary-300","className":"is-style-default","fontSize":"14"} -->
+<p class="aligncenter has-text-align-center is-style-default has-primary-300-color has-text-color has-link-color has-14-font-size aligncenter">Trusted by the best brands</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+<!-- wp:group {"align":"wide","style":{"u002du002dmarquee-speed-mobile":"50s","u002du002dmarquee-speed-desktop":"50s","u002du002dmarquee-direction":"forwards","u002du002dmarquee-pause":"paused","spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|md","right":"0","bottom":"var:preset|spacing|sm"},"blockGap":"var:preset|spacing|xl"},"\u002d\u002dmarquee-speed-mobile":"50s","\u002d\u002dmarquee-speed-desktop":"50s","\u002d\u002dmarquee-direction":"forwards","\u002d\u002dmarquee-pause":"running"},"layout":{"type":"flex","flexWrap":"nowrap","orientation":"marquee","justifyContent":"center"},"speedMobile":"50","speedDesktop":"50","reverse":false,"pauseOnHover":false,"repeatItems":1,"fadeEdges":true} -->
+<div class="wp-block-group alignwide fade-edges" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--md);padding-right:0;padding-bottom:var(--wp--preset--spacing--sm);--marquee-speed-mobile:50s;--marquee-speed-desktop:50s;--marquee-direction:forwards;--marquee-pause:running"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xl"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":15385,"width":"140px","sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"0px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border" style="border-radius:0px"><img src="<?php echo content_url( "/themes/codeify/assets/" ) ?>svg/wireframewp.svg" alt="" class="wp-image-15385" style="border-radius:0px;width:140px"/></figure>
+<!-- /wp:image -->
+<!-- wp:image {"id":15381,"width":"140px","sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"0px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border" style="border-radius:0px"><img src="<?php echo content_url( "/themes/codeify/assets/" ) ?>svg/optimizewp.svg" alt="" class="wp-image-15381" style="border-radius:0px;width:140px"/></figure>
+<!-- /wp:image -->
+<!-- wp:image {"id":15380,"width":"140px","sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"0px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border" style="border-radius:0px"><img src="<?php echo content_url( "/themes/codeify/assets/" ) ?>svg/convertpress.svg" alt="" class="wp-image-15380" style="border-radius:0px;width:140px"/></figure>
+<!-- /wp:image -->
+<!-- wp:image {"id":15378,"width":"140px","sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"0px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border" style="border-radius:0px"><img src="<?php echo content_url( "/themes/codeify/assets/" ) ?>svg/block-library.svg" alt="" class="wp-image-15378" style="border-radius:0px;width:140px"/></figure>
+<!-- /wp:image -->
+<!-- wp:image {"id":15404,"width":"100px","sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"0px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border" style="border-radius:0px"><img src="<?php echo content_url( "/themes/codeify/assets/" ) ?>svg/blockify.svg" alt="" class="wp-image-15404" style="border-radius:0px;width:100px"/></figure>
+<!-- /wp:image -->
+<!-- wp:image {"id":15406,"width":"100px","sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"0px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border" style="border-radius:0px"><img src="<?php echo content_url( "/themes/codeify/assets/" ) ?>svg/sellgrid.svg" alt="" class="wp-image-15406" style="border-radius:0px;width:100px"/></figure>
+<!-- /wp:image -->
+<!-- wp:image {"id":15418,"width":"120px","sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"0px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border" style="border-radius:0px"><img src="<?php echo content_url( "/themes/codeify/assets/" ) ?>svg/arraypress.svg" alt="" class="wp-image-15418" style="border-radius:0px;width:120px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
